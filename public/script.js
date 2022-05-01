5baa99db05c28dda674c958ba1be021744441438
jinyan <moralyplin@gmail.com>
2022-05-01 16:44:28 +0800

    fix
    fix fetch for json stringify

--- public/script.js
+++ public/script.js
@@ -1,12 +1,15 @@
-const { json } = require("express/lib/response");
 
 const amountElement = document.getElementById('amount');
 
 paypal.Buttons({
-    createOrder: function(data, actions) {
+    createOrder: function() {
       // This function sets up the details of the transaction, including the amount and line item details.
       return fetch('/create-order', {
+        //upload json data
         method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
         body: JSON.stringify({
           items: [{
             id: 1,
@@ -15,13 +18,18 @@ paypal.Buttons({
         {
           id: 2,
           quantity: 3}]
-        }).then(res => {
-          if(res.ok)
-            return res.json(); 
-          return res.json().then(json => Promise.reject(json))
         })
       })
-    },
+        .then(res => {
+          if(res.ok)
+            return res.json(); 
+          return res.json().then(json => Promise.reject(json))  //return for reject message of promise
+        }).then(({ id }) => {
+          return id;
+        }).catch( (e) => {
+          console.error(e.error);
+        })
+      },
     onApprove: function(data, actions) {
       // This function captures the funds from the transaction.
       return actions.order.capture().then(function(details) {
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const paypal = require('@paypal/checkout-server-sdk');
+const { Template } = require('ejs');
 
 const storeItems = new Map([
     [1, {price: 100, name: "paypal one"}],
@@ -14,6 +16,40 @@ app.get('/', (req,res) => {
     res.render('index', { paypalClientId: process.env.PAYPAL_CLIENT_ID});
 });
 
-
+//post
+app.post('/create-order', (req,res) => {
+    const request = new paypal.orders.OrdersCreateRequest();
+    const total = req.body.items.reduce((sum, item) => {
+        return sum + storeItems.get(item.id).price * quantity;
+    })
+    request.requestBody({
+        intent: 'CAPTURE',
+        purchase_units:[
+            {
+                amount: {
+                    currency_code: 'USD',
+                    value: total,
+                    breakdown:{
+                        item_total: {
+                            currency_code: 'USD',
+                            value: total
+                        }
+                    }
+                },
+                items: req.body.items.Map(item => {
+                    const storeItem = storeItems.get(item.id)
+                    return {
+                        name: storeItem.name,
+                        unit_amount: {
+                            currency_code: 'USD',
+                            value: storeItem.price
+                        },
+                        quantity: item.quantity
+                    }
+                })
+            }
+        ]
+    })
+})
 
 app.listen(3000);
